113fbd6247ff1244014234f2347319751acb11fe
Ben England <bengland@redhat.com>
2019-09-12 16:05:25 -0400

    CI cleanups
    provide explicit failure message if fs-drift ansible task did not finish
    exit with error status if ansible task did not finish
    increase sleep time to rule out possibility that test was slow starting up

--- tests/test_fs_drift.sh
+++ tests/test_fs_drift.sh
@@ -5,7 +5,7 @@ source tests/common.sh
 
 function finish {
   echo "Cleaning up fs-drift"
-  kubectl delete -f tests/test_crs/valid_fs_drift.yaml
+  kubectl delete -f tests/test_crs/valid_fs_drift.yaml || echo CR delete did not happen
   delete_operator
 }
 
@@ -23,9 +23,10 @@ function functional_test_fs_drift {
     --namespace my-ripsaw --timeout=200s" "200s" $fsdrift_pod
   wait_for "kubectl wait --for=condition=complete -l app=fs-drift-benchmark-$uuid jobs \
     --namespace my-ripsaw --timeout=100s" "200s" $fsdrift_pod
-  sleep 20
-  # ensuring the run has actually happened
-  kubectl logs "$fsdrift_pod" --namespace my-ripsaw | grep "RUN STATUS"
+  sleep 30
+  # if the run did not complete successfully, tell them so
+  (kubectl logs "$fsdrift_pod" --namespace my-ripsaw | grep "RUN STATUS DONE") \
+  || (echo 'fs-drift test: FAILED' ; exit 1)
   echo "fs-drift test: Success"
 }
 
